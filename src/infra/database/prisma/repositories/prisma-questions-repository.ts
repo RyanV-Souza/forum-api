a984007c41a0af8c425c0f4385e81d15767e2b62
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-09-14 20:12:29 -0300

    feat: implements to persistence mapper

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -22,22 +22,49 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
   }
 
   async findBySlug(slug: string) {
-    throw new Error("Method not implemented.");
+    const question = await this.prisma.question.findUnique({
+      where: { slug },
+    });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
   }
 
-  async findManyRecent(params: PaginationParams) {
-    throw new Error("Method not implemented.");
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = await this.prisma.question.findMany({
+      orderBy: { createdAt: "desc" },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return questions.map(PrismaQuestionMapper.toDomain);
   }
 
   async save(question: Question) {
-    throw new Error("Method not implemented.");
+    const data = PrismaQuestionMapper.toPersistence(question);
+
+    await this.prisma.question.update({
+      where: { id: question.id.toString() },
+      data,
+    });
   }
 
   async create(question: Question) {
-    throw new Error("Method not implemented.");
+    const data = PrismaQuestionMapper.toPersistence(question);
+
+    await this.prisma.question.create({
+      data,
+    });
   }
 
   async delete(question: Question) {
-    throw new Error("Method not implemented.");
+    const data = PrismaQuestionMapper.toPersistence(question);
+
+    await this.prisma.question.delete({
+      where: { id: data.id },
+    });
   }
 }
